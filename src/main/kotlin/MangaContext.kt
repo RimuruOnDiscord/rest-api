80135e1042dbc67b4428a1ffe0fdf8ca6f72be63
Rimuru <velocityontopfrfr@gmail.com>
2026-02-03 22:05:18 +0800

    Update MangaContext.kt

--- src/main/kotlin/MangaContext.kt
+++ src/main/kotlin/MangaContext.kt
@@ -1,14 +1,13 @@
-import okhttp3.OkHttpClient
 import okhttp3.CookieJar
+import okhttp3.OkHttpClient
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
-import org.koitharu.kotatsu.parsers.MangaSourceConfig
 import org.koitharu.kotatsu.parsers.MangaParserSource
+import org.koitharu.kotatsu.parsers.MangaSourceConfig
 
 class WebMangaContext : MangaLoaderContext {
     override val httpClient: OkHttpClient = OkHttpClient.Builder().build()
     override val cookieJar: CookieJar = CookieJar.NO_COOKIES
     
-    // Just returns a default config for every source
     override fun getParserConfig(source: MangaParserSource): MangaSourceConfig {
         return object : MangaSourceConfig {}
     }
