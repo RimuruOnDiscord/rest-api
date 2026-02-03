3383cf29353d51149713c07cd66ddb9f7200aa21
Rimuru <velocityontopfrfr@gmail.com>
2026-02-03 22:04:48 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,15 +2,39 @@ plugins {
     kotlin("jvm") version "1.9.20"
     kotlin("plugin.serialization") version "1.9.20"
     id("io.ktor.plugin") version "2.3.5"
-    id("com.github.johnrengelman.shadow") version "8.1.1" 
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
+
+group = "com.mangavel"
+version = "1.0.0"
+
 repositories {
     mavenCentral()
     maven { url = uri("https://jitpack.io") }
 }
 
+dependencies {
+    // The Scraper Library
+    implementation("com.github.KotatsuApp:kotatsu-parsers:2.22.2")
+    
+    // Ktor Server Core & Netty
+    implementation("io.ktor:ktor-server-core-jvm:2.3.5")
+    implementation("io.ktor:ktor-server-netty-jvm:2.3.5")
+    
+    // JSON & CORS
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:2.3.5")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:2.3.5")
+    implementation("io.ktor:ktor-server-cors-jvm:2.3.5")
+
+    // Logging
+    implementation("ch.qos.logback:logback-classic:1.4.11")
+}
+
 tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
     manifest {
-        attributes["Main-Class"] = "ApplicationKt" // Make sure this matches your file name
+        attributes["Main-Class"] = "ApplicationKt"
     }
+    archiveBaseName.set("manga-api")
+    archiveClassifier.set("all")
+    archiveVersion.set("")
 }
